8e10824c59e8cce2a22f4c61100cd18aa4a86f36
riataso <nkc20212452@denpa.jp>
2022-03-19 21:45:23 +0900

    first commit

---
+++ LS.js
@@ -0,0 +1,36 @@
+const taskValue = document.getElementsByClassName('task_text')[0];
+const taskSubmit = document.getElementsByClassName('task_submit')[0];
+const taskList = document.getElementsByClassName('task_list')[0];
+
+
+const addTasks = (task) => {
+
+const listItem = document.createElement('li');
+const showItem = taskList.appendChild(listItem);
+showItem.innerHTML = task;
+
+
+const deleteButton = document.createElement('button');
+deleteButton.innerHTML = 'Delete';
+listItem.appendChild(deleteButton);
+
+
+deleteButton.addEventListener('click', e => {
+e.preventDefault();
+deleteTasks(deleteButton);
+});
+};
+
+
+const deleteTasks = (deleteButton) => {
+const chosenTask = deleteButton.closest('li');
+taskList.removeChild(chosenTask);
+};
+
+
+taskSubmit.addEventListener('click', e => {
+e.preventDefault();
+const task = taskValue.value;
+addTasks(task);
+taskValue.value = '';
+});
