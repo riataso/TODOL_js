d613d06653cd2f3b17c4529fd95fe074f59819ae
riataso <nkc20212452@denpa.jp>
2022-03-26 16:57:29 +0900

    キーボードからの操作

--- LS.js
+++ LS.js
@@ -1,6 +1,41 @@
 const taskValue = document.getElementsByClassName('task_text')[0];
 const taskSubmit = document.getElementsByClassName('task_submit')[0];
 const taskList = document.getElementsByClassName('task_list')[0];
+const form = document.getElementsByClassName('task_text')[0];
+const input = document.getElementById('input');
+
+
+
+input.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(form.value);
+    pitem();
+});
+
+const  pitem =() =>{ 
+    let todoList = form.value;
+    if(todoList.length){
+        const liItem = document.createElement('li');
+        liItem.innerHTML = form.value;
+        ul.appendChild(liItem);
+        form.value="";
+
+        const deleteList = document.createElement('button');
+        deleteList.innerHTML = 'Delete' ;
+        liItem.appendChild(deleteList);
+
+        liItem.addEventListener('contextmenu', e =>{
+            e.preventDefault();
+            liItem.remove();
+        });
+
+        deleteList.addEventListener('click' , e =>{
+            e.preventDefault();
+            deleteTasks(deleteList);
+        });
+    };
+};
+
 
 
 const addTasks = (task) => {
@@ -16,8 +51,14 @@ listItem.appendChild(deleteButton);
 
 
 deleteButton.addEventListener('click', e => {
-e.preventDefault();
-deleteTasks(deleteButton);
+    e.preventDefault();
+    deleteTasks(deleteButton);
+
+llistItem.addEventListener('contextmenu', e =>{
+    e.preventDefault();
+    listItem.remove();
+});
+
 });
 };
 
@@ -29,8 +70,12 @@ taskList.removeChild(chosenTask);
 
 
 taskSubmit.addEventListener('click', e => {
-e.preventDefault();
-const task = taskValue.value;
-addTasks(task);
-taskValue.value = '';
+        e.preventDefault();
+        console.log(taskValue.value);
+        const task = taskValue.value;
+        if (task){
+            addTasks(task);
+            taskValue.value = '';
+        };
 });
+
